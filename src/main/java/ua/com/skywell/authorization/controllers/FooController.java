7a90ac6660cfc4b4aadc2fa603d141969a5c6cdd
igor.vartanian <igor.vartanian@gmail.com>
2016-09-06 00:47:47 +0300

    auth

--- src/main/java/ua/com/skywell/authorization/controllers/FooController.java
+++ src/main/java/ua/com/skywell/authorization/controllers/FooController.java
@@ -16,13 +16,13 @@ import static org.apache.commons.lang3.RandomStringUtils.randomNumeric;
 @RestController
 public class FooController {
 
-//    @PreAuthorize("#oauth2.hasScope('read')")
+    @PreAuthorize("#oauth2.hasScope('read')")
     @RequestMapping(method = RequestMethod.GET, value = "/foos/{id}")
     public FooDto findById(@PathVariable long id) {
         return new FooDto(id, randomAlphabetic(4));
     }
 
-//    @PreAuthorize("#oauth2.hasScope('write')")
+    @PreAuthorize("#oauth2.hasScope('write')")
     @RequestMapping(method = RequestMethod.POST, value = "/foos")
     public FooDto create(@RequestBody final FooDto foo) {
         foo.setId(Long.parseLong(randomNumeric(2)));
